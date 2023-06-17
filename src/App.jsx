738e334eaccec6d8be0d2b9c53ceb18b9d1406b0
daoraCode <severin.mboukou@gmail.com>
2023-06-17 11:03:14 +0200

    working on auth forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react"
+import { SignupForm } from "./component/Forms/SingnuForm"
 
-function App() {
+const App = () => {
   return (
     <div>
-      <h1>hello, world!</h1>
+      <SignupForm />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
