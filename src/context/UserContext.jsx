4116dd076498d2545086063acb5383635bc551bb
daoraCode <severin.mboukou@gmail.com>
2023-06-17 13:56:25 +0200

    regex password added for signup form

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,31 +1,35 @@
-import { createContext, useEffect, useState } from "react";
-// api call getMe()
-import { getMe } from "../api/auth";
+import { createContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
 
-const UserContext = createContext({});
+const UserContext = createContext({})
 
-const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+const UserContextProvider = ({ children }) => {
+  const navigate = useNavigate()
+  const [user, setUser] = useState(null)
 
-  // api call
-  useEffect(() => {
-    getMe();
-  }, []);
-
-  const getUser = async () => {
-    const fetchedUser = await getMe();
-    if (!fetchedUser.error) {
-      setUser(fetchedUser);
-    }
-  };
+  //signup
+  const signup = async (values) => {
+    const res = await fetch("http://localhost:4004/api/user/auth/signup", {
+      method: "post",
+      headers: {
+        'Content-type': 'application/json"
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        username: value.username,
+        email: value.email,
+        password: value.password,
+      })
+    })
+  }
 
   const value = {
     user,
     setUser,
     getUser,
-  };
+  }
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
 
-export { UserContext, UserProvider };
+export { UserContext, UserContextProvider }
--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -5,11 +5,7 @@ import "./signup.css"
 // a custom valisation function.
 const validate = (values) => {
   const errors = {}
-  if (!values.firstName) {
-    errors.firstName = "Champs requis. Veuillez insérer votre prénom."
-  } else if (values.firstName.length > 15) {
-    errors.firstName = "Maximum 15 caractères ou moins est requis."
-  }
+  let passwordRegex = /(?=.*[0-9])/
 
   if (!values.username) {
     errors.username = "Champs requis. Veuillez insérer un surnom."
@@ -24,6 +20,25 @@ const validate = (values) => {
     errors.email = "Adresse e-mail invalide."
   }
 
+  if (!values.password) {
+    errors.password = "Champs requis. Veuillez définir un mot de passe."
+  } else if (values.password.length < 4) {
+    errors.password = "Min. 8 caractères pour définir un mot passe."
+  } else if (values.password.length > 9) {
+    errors.password = "Max. 9 caractères pour définir un mot passe."
+  } else if (!passwordRegex.test(values.password)) {
+    errors.password = "Le mot doit contenir au moins un chiffre."
+  }
+
+  // const validateConfirmPassword = (pass, value) => {
+
+  //   let error = "";
+  //   if (pass && value) {
+  //     if (pass !== value) {
+  //       error = "Password not matched";
+  //     }
+  // }
+
   return errors
 }
 
@@ -32,12 +47,13 @@ export const Signup = () => {
   // be called when the form is submitted
   const formik = useFormik({
     initialValues: {
-      firstName: "",
       username: "",
       email: "",
+      password: "",
     },
     validate,
     onSubmit: (values) => {
+      console.log(values)
       alert(JSON.stringify(values, null, 2))
     },
   })
@@ -46,23 +62,6 @@ export const Signup = () => {
     <div className="main-su-form">
       <h3 className="heading-text-su-form">AUTHENTIFICATION</h3>
       <form className="su-form" onSubmit={formik.handleSubmit}>
-        <label className="su-form-label" htmlFor="firstName">
-          Prénom
-        </label>
-        <input
-          placeholder="Jon Snow"
-          className="input-su-form"
-          id="firstName"
-          name="firstName"
-          type="text"
-          onBlur={formik.handleBlur}
-          onChange={formik.handleChange}
-          value={formik.values.firstName}
-        />
-        {formik.touched.firstName && formik.errors.firstName ? (
-          <div className="su-form-error-alert">{formik.errors.firstName}</div>
-        ) : null}
-
         <label className="su-form-label" htmlFor="username">
           Surnom
         </label>
@@ -84,7 +83,7 @@ export const Signup = () => {
           Adresse e-mail
         </label>
         <input
-          placeholder="jonsnow@got.com"
+          placeholder="jonsnow@hbo.com"
           className="input-su-form"
           id="email"
           name="email"
@@ -95,6 +94,23 @@ export const Signup = () => {
         />
         {formik.touched.email && formik.errors.email ? (
           <div className="su-form-error-alert">{formik.errors.email}</div>
+        ) : null}
+
+        <label className="su-form-label" htmlFor="password">
+          Mot de passe
+        </label>
+        <input
+          placeholder="************"
+          className="input-su-form"
+          id="password"
+          name="password"
+          type="password"
+          onBlur={formik.handleBlur}
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="su-form-error-alert">{formik.errors.password}</div>
         ) : null}
 
         <button className="su-form-btn" type="submit">
