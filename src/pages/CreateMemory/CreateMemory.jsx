0b0cbcd7aa67881bd4112328243e586e938d5991
daoraCode <severin.mboukou@gmail.com>
2023-06-21 11:32:02 +0200

    memory created into db

--- src/pages/CreateMemory/CreateMemory.jsx
+++ src/pages/CreateMemory/CreateMemory.jsx
@@ -2,33 +2,59 @@ import { useState } from "react"
 import { useFormik } from "formik"
 import "./creatememory.css"
 
-// formulaire de création de souvenir basée sur un endpoint
+// form to create a memory
 
 export const CreateMemory = () => {
-  // const formik = useFormik({})
+  // endpoint create a memory comiing from backend API
+  const urlMemory = "http://localhost:4004/api/memories/memory/create"
+  const token = sessionStorage.setItem("token")
+  // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0OTI4MGUzM2ViNmM2NjU1ZjNiNDhiYSIsImlhdCI6MTY4NzMzOTA3MCwiZXhwIjoxNjg3MzQwNDcwfQ.5WpiGaoaxZnYoLU4lLGv_jvghQhBgbuhctGPbL9ktaw"
 
-  const [memory, setMemory] = useState({
-    theme: "",
-    presention: "",
-    content: "",
+  const formik = useFormik({
+    initialValues: {
+      theme: "",
+      presention: "",
+      content: "",
+    },
+    // validate,
+    onSubmit: async (values) => {
+      console.log(values)
+      const fetchedCreateMemory = await fetch(urlMemory, {
+        method: "post",
+
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`, // <-- user's token from cookie his session
+        },
+        body: JSON.stringify({
+          theme: values.theme,
+          presentation: values.presentation,
+          content: values.content,
+        }),
+      })
+    },
   })
+
+  // const [memory, setMemory] = useState({
+  // })
 
   return (
     <main className="main-c-memory-ctn">
       <h1 className="heading-c-memory">Souvenirs</h1>
       <div className="ctn">
         {/* <h3>CRÉER VOTRE SOUVENIRS DE FILMS</h3> */}
-        <form onSubmit={}>
+        <form onSubmit={formik.handleSubmit}>
           <div className="ctn-c-memory-theme">
             <label className="label-theme">
               Thème de votre souvenir de film / scène préférée ⚡️
             </label>
             <input
               className="input-theme"
-              placeholder="Le thème / le titre de votre souvenir..."
+              placeholder="Le thème / le titre de souvenir..."
               id="theme"
               name="theme"
               type="text"
+              onChange={formik.handleChange}
             />
           </div>
 
@@ -39,9 +65,10 @@ export const CreateMemory = () => {
             <input
               className="input-presentation"
               placeholder="Image de souvenir..."
-              id="souvenir"
-              name="souvenir"
+              id="presentation"
+              name="presentation"
               type="text"
+              onChange={formik.handleChange}
             />
           </div>
 
@@ -51,10 +78,11 @@ export const CreateMemory = () => {
             </label>
             <input
               className="input-content"
-              placeholder="Mon souvenir..."
+              placeholder="Mon souvenir à décrire..."
               id="content"
               name="content"
               type="text"
+              onChange={formik.handleChange}
             />
           </div>
 
