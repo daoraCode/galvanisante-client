e48054995374ce6f91cdfca45c50f0c0019eb4a9
daoraCode <severin.mboukou@gmail.com>
2023-06-24 07:07:14 +0200

    renaming page directories

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { UserContextProvider } from './contexts/UserContext'
 import { MemoryContextProvider } from './contexts/MemoryContext'
 
-// import { SignUp } from './pages/signup/SignUp'
-// import { Login } from './pages/login/Login'
+import { SignUp } from './pages/signup/SignUp'
+import { Login } from './pages/login/Login'
 import { CreateMemory } from './pages/creatememory/CreateMemory'
 import { Home } from './pages/home/Home'
 
@@ -20,8 +20,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route exact path="/" element={<Home />} />
-            {/* <Route path="/signup" element={<SignUp />} /> */}
-            {/* <Route path="/login" element={<Login />} /> */}
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/create-memory" element={<CreateMemory />} />
           </Routes>
         </MemoryContextProvider>
