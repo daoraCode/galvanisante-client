7ac3d8d34a53ef194ed7fb3864ebae8df6102466
Severin Mb <47704495+daoraCode@users.noreply.github.com>
2023-09-11 22:03:43 +0200

    fixing deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import React from 'react'
+// import React from 'react'
 
-import { Routes, Route } from 'react-router-dom'
+// import { Routes, Route } from 'react-router-dom'
 
-import { UserContextProvider } from './contexts/UserContext'
-import { MemoryContextProvider } from './contexts/MemoryContext'
+// import { UserContextProvider } from './contexts/UserContext'
+// import { MemoryContextProvider } from './contexts/MemoryContext'
 
-import { Header } from './components/Header/Header'
+// import { Header } from './components/Header/Header'
 
-import { Home } from './pages/home/Home'
-import { SignUp } from './pages/signup/SignUp'
-import { Login } from './pages/login/Login'
-import { CreateMemory } from './pages/creatememory/CreateMemory'
-import { Memory } from './pages/memory/Memory'
+// import { Home } from './pages/home/Home'
+// import { SignUp } from './pages/signup/SignUp'
+// import { Login } from './pages/login/Login'
+// import { Memory } from './pages/memory/Memory'
+// import { CreateMemory } from './pages/creatememory/CreateMemory'
+
+// const App = () => {
+//   return (
+//     <MemoryContextProvider>
+//       <UserContextProvider>
+//         <Header />
+//         <Routes>
+//           <Route exact path="/" element={<Home />} />
+//           <Route path="/signup" element={<SignUp />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/memory/:id" element={<Memory />} />
+//           <Route path="/create-memory" element={<CreateMemory />} />
+//           {/* <Route path="*" element={<NotFound />} /> */}
+//         </Routes>
+//       </UserContextProvider>
+//     </MemoryContextProvider>
+//   )
+// }
+
+// export default App
 
 
-const App = () => {
-  return (
-    <MemoryContextProvider>
-      <UserContextProvider>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/memory/:id" element={<Memory />} />
-          <Route path="/create-memory" element={<CreateMemory />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-      </UserContextProvider>
-    </MemoryContextProvider>
-  )
-}
 
-export default App
+
+
+
+
+
